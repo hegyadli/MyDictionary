376b0d9c0510a021b7d0407a4726ebe99db2f74e
hegyadli <77647185+hegyadli@users.noreply.github.com>
2021-02-11 13:51:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Kerem Koca");
+            myDictionary.Add(2, "Yiğit Özkaya");
+            myDictionary.Add(3, "Sadık Emre Polat");
+
+            myDictionary.ToList();
+            Console.WriteLine(myDictionary.Count());
+        }
+    }
+}
