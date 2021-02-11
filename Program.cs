c414232aca821a0d720547072f53b16f1f7bcdb8
hegyadli <77647185+hegyadli@users.noreply.github.com>
2021-02-11 12:58:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "One");
+            Console.WriteLine(myDictionary.Length + "," + myDictionary.Length1);
+            myDictionary.Add(2, "Two");
+            Console.WriteLine(myDictionary.Length + "," + myDictionary.Length1);
+            myDictionary.Add(3, "Three");
+            Console.WriteLine(myDictionary.Length + "," + myDictionary.Length1);
+            myDictionary.Add(4, "Four");
+            myDictionary.Add(5, "Five");
+        }
+    }
+}
